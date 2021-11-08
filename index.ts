f630c2b33e4db745d49cdb2311c27eb49d6e5aa6
keithchasen <keithchasen89@gmail.com>
2021-11-08 12:59:07 +0200

    basic usage of non type vars

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello')
+const name = "Kostya",
+    age = 32,
+    gender = 'man';
+
+const sayHi = (name, age, gender?) => console.log(`Hello ${name}, you are ${age} and you are a ${gender}`);
+
+sayHi(name, age, gender);
+
+export {};
